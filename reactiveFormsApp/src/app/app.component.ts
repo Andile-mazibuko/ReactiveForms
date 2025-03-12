2c2c8e27ebea0f32e1f79bed629abbd4500c9ff4
Andile <andilelethumusa@gmail.com>
2025-03-12 15:35:19 +0200

    Form groups created

--- reactiveFormsApp/src/app/app.component.ts
+++ reactiveFormsApp/src/app/app.component.ts
@@ -9,14 +9,29 @@ import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 })
 export class AppComponent {
   userForm: FormGroup;
+  title: string = 'Rective forms';
 
   constructor(private formBuilder: FormBuilder)
   { 
+    //Every component related to this form must be inside the userForm group
     //Creates a form group with none nullable inputs
     this.userForm = this.formBuilder.group({
       name: ['',Validators.required],
-      Email: ['', Validators.required]
- 
+      Email: ['', [Validators.required, Validators.email ]],
+      
+      //Nested groups
+      address: this.formBuilder.group({
+        province: ['',Validators.required],
+        postCode: ['', Validators.required]//ToDo: set length and accept number only
+      
+      }),
+
+      //FormArray
+      // .array method only accepts the square brackets since it's a array
+      phoneNumbers: this.formBuilder.array([
+        this.formBuilder.control('',Validators.required)//no square brackets here or curly brackets
+      ])
+
     });
     
   }
